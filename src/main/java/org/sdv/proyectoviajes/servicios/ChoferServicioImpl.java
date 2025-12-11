632c16aaff87abf8dca5d50dcb8561987e5f518a
SamuelDV <dominguezvirelassamuel13@gmail.com>
2025-12-10 22:29:20 -0600

    feat: Implementar Servicio para choferes

---
+++ src/main/java/org/sdv/proyectoviajes/servicios/ChoferServicioImpl.java
@@ -0,0 +1,52 @@
+package org.sdv.proyectoviajes.servicios;
+
+import jakarta.inject.Inject;
+import org.sdv.proyectoviajes.excepciones.ServicioException;
+import org.sdv.proyectoviajes.modelos.Chofer;
+import org.sdv.proyectoviajes.repositorios.ChoferRepositorio;
+import org.sdv.proyectoviajes.repositorios.Repositorio;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ChoferServicioImpl implements ServicioChoferes{
+
+    @Inject
+    private Repositorio<Chofer> choferRepositorio;
+
+    @Override
+    public void guardarChofer(Chofer chofer) {
+        try{
+            choferRepositorio.guardar(chofer);
+        }catch (SQLException e){
+            throw new ServicioException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void eliminarChofer(Long id) {
+        try{
+            choferRepositorio.eliminar(id);
+        }catch (SQLException e){
+            throw new ServicioException(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<Chofer> buscarTodosChoferes() {
+        try{
+            return choferRepositorio.buscarTodos();
+        }catch (SQLException e){
+            throw new ServicioException(e.getMessage());
+        }
+    }
+
+    @Override
+    public Chofer buscarPorIdChofer(Long id) {
+        try{
+            return choferRepositorio.buscarPorId(id);
+        }catch (SQLException e){
+            throw new ServicioException(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/org/sdv/proyectoviajes/servicios/ServicioChoferes.java
@@ -0,0 +1,12 @@
+package org.sdv.proyectoviajes.servicios;
+
+import org.sdv.proyectoviajes.modelos.Chofer;
+
+import java.util.List;
+
+public interface ServicioChoferes {
+    void guardarChofer(Chofer chofer);
+    void eliminarChofer(Long id);
+    List<Chofer> buscarTodosChoferes();
+    Chofer buscarPorIdChofer(Long id);
+}
