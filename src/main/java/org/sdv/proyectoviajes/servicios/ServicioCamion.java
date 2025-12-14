7d51975c9594195904aa5263b1a6058daad9f4f4
SamuelDV <dominguezvirelassamuel13@gmail.com>
2025-12-13 18:50:25 -0600

    feat: Implementar servicio para camiones

---
+++ src/main/java/org/sdv/proyectoviajes/servicios/ServicioCamion.java
@@ -0,0 +1,12 @@
+package org.sdv.proyectoviajes.servicios;
+
+import org.sdv.proyectoviajes.modelos.Camion;
+
+import java.util.List;
+
+public interface ServicioCamion {
+    void guardarCamion(Camion camion);
+    void eliminarCamion(Long id);
+    List<Camion> buscarTodosCamiones();
+    Camion buscarPorIdCamion(Long id);
+}
---
+++ src/main/java/org/sdv/proyectoviajes/servicios/ServicioCamionImpl.java
@@ -0,0 +1,51 @@
+package org.sdv.proyectoviajes.servicios;
+
+import jakarta.inject.Inject;
+import org.sdv.proyectoviajes.excepciones.ServicioException;
+import org.sdv.proyectoviajes.modelos.Camion;
+import org.sdv.proyectoviajes.repositorios.Repositorio;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ServicioCamionImpl implements ServicioCamion {
+
+    @Inject
+    private Repositorio<Camion> repositorioCamion;
+
+    @Override
+    public void guardarCamion(Camion camion) {
+        try{
+            repositorioCamion.guardar(camion);
+        }catch (SQLException e){
+            throw new ServicioException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void eliminarCamion(Long id) {
+        try{
+            repositorioCamion.eliminar(id);
+        }catch (SQLException e){
+            throw new ServicioException(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<Camion> buscarTodosCamiones() {
+        try{
+            return repositorioCamion.buscarTodos();
+        }catch (SQLException e){
+            throw new ServicioException(e.getMessage());
+        }
+    }
+
+    @Override
+    public Camion buscarPorIdCamion(Long id) {
+        try{
+            return repositorioCamion.buscarPorId(id);
+        }catch (SQLException e){
+            throw new ServicioException(e.getMessage());
+        }
+    }
+}
