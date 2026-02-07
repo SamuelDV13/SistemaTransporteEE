43d65a543e56e678a08e4d88ebcb545cf0699a4b
SamuelDV <dominguezvirelassamuel13@gmail.com>
2026-02-06 20:29:42 -0600

    feat: Implementar el cambio de estados dentro del ServicioViajeImpl en el metodo guardar

--- src/main/java/org/sdv/proyectoviajes/servicios/ServicioViajeImpl.java
+++ src/main/java/org/sdv/proyectoviajes/servicios/ServicioViajeImpl.java
@@ -3,8 +3,14 @@ package org.sdv.proyectoviajes.servicios;
 import jakarta.inject.Inject;
 import org.sdv.proyectoviajes.config.AServicio;
 import org.sdv.proyectoviajes.excepciones.ServicioException;
+import org.sdv.proyectoviajes.modelos.Camion;
+import org.sdv.proyectoviajes.modelos.Chofer;
 import org.sdv.proyectoviajes.modelos.Viaje;
+import org.sdv.proyectoviajes.modelos.enumeradores.EstadosCamion;
+import org.sdv.proyectoviajes.modelos.enumeradores.EstadosChofer;
 import org.sdv.proyectoviajes.repositorios.Repositorio;
+import org.sdv.proyectoviajes.repositorios.RepositorioCamion;
+import org.sdv.proyectoviajes.repositorios.RepositorioChoferImpl;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -15,10 +21,18 @@ public class ServicioViajeImpl implements ServicioViajes {
     @Inject
     private Repositorio<Viaje> viajeRepositorio;
 
+    @Inject
+    private RepositorioCamion camionRepositorio;
+
+    @Inject
+    private RepositorioChoferImpl choferRepositorio;
+
     @Override
     public void guardarViaje(Viaje viaje) {
         try {
             viajeRepositorio.guardar(viaje);
+            camionRepositorio.actualizarEstado(viaje.getCamion().getId(), EstadosCamion.EN_VIAJE.toString());
+            choferRepositorio.actualizarEstado(viaje.getChofer().getId(), EstadosChofer.EN_VIAJE.toString());
         } catch (SQLException e) {
             throw new ServicioException(e.getMessage());
         }
