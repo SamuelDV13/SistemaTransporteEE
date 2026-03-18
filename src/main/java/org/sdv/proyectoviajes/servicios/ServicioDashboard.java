b9767d6052772d2e606995b4287e93fbc89f663a
SamuelDV <dominguezvirelassamuel13@gmail.com>
2026-03-18 13:19:02 -0600

    feat: implementar servicio para el Dashboard

---
+++ src/main/java/org/sdv/proyectoviajes/servicios/ServicioDashboard.java
@@ -0,0 +1,7 @@
+package org.sdv.proyectoviajes.servicios;
+
+import org.sdv.proyectoviajes.dto.DashboardDto;
+
+public interface ServicioDashboard {
+    DashboardDto obtenerMetricas();
+}
---
+++ src/main/java/org/sdv/proyectoviajes/servicios/ServicioDashboardImpl.java
@@ -0,0 +1,43 @@
+package org.sdv.proyectoviajes.servicios;
+
+import jakarta.inject.Inject;
+import org.sdv.proyectoviajes.dto.DashboardDto;
+import org.sdv.proyectoviajes.excepciones.ServicioException;
+import org.sdv.proyectoviajes.repositorios.RepositorioCamion;
+import org.sdv.proyectoviajes.repositorios.RepositorioChoferes;
+import org.sdv.proyectoviajes.repositorios.RepositorioViaje;
+
+import java.math.BigDecimal;
+import java.sql.SQLException;
+
+public class ServicioDashboardImpl implements ServicioDashboard {
+
+    @Inject
+    private RepositorioViaje repositorioViaje;
+
+    @Inject
+    private RepositorioChoferes repositorioChoferes;
+
+    @Inject
+    private RepositorioCamion repositorioCamion;
+
+    @Override
+    public DashboardDto obtenerMetricas() {
+
+        DashboardDto dashboardDto = new DashboardDto();
+
+        try {
+            dashboardDto.setViajesEnRuta(repositorioViaje.contarPorEstado("EN_PROCESO"));
+            dashboardDto.setCamionesDisponibles(repositorioCamion.contarPorEstado("DISPONIBLE"));
+            dashboardDto.setChoferesLibres(repositorioChoferes.contarPorEstado("DISPONIBLE"));
+
+            BigDecimal dinero = repositorioViaje.obtenerGananciasMensuales();
+            dashboardDto.setGananciasMes((dinero != null) ? dinero : BigDecimal.ZERO);
+
+        }catch(SQLException e){
+            throw new ServicioException(e.getMessage());
+        }
+
+        return dashboardDto;
+    }
+}
