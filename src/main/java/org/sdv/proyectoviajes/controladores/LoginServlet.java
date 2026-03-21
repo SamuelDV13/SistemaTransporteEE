353ecc79e1d1b792dd1e985884b5e4b0fc93f067
SamuelDV <dominguezvirelassamuel13@gmail.com>
2026-03-20 18:40:28 -0600

    feat: Implementar controlador para iniciar sesion

---
+++ src/main/java/org/sdv/proyectoviajes/controladores/LoginServlet.java
@@ -0,0 +1,50 @@
+package org.sdv.proyectoviajes.controladores;
+
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.sdv.proyectoviajes.modelos.Usuario;
+import org.sdv.proyectoviajes.servicios.ServicioUsuarios;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Inject
+    private ServicioUsuarios servicioUsuarios;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("login.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        Usuario usuario = servicioUsuarios.obtenerPorCredenciales(username, password);
+
+        if (usuario != null) {
+            //Logica para permitir el acceso
+
+            HttpSession session = req.getSession();
+            session.setAttribute("usuario", usuario);
+
+            resp.sendRedirect(req.getContextPath() + "/dashboard");
+
+        } else{
+            //Logica para denegar el acceso
+
+            req.setAttribute("error", "Usuario o contraseña incorrectos.");
+            req.getRequestDispatcher("/login.jsp").forward(req, resp);
+        }
+
+    }
+}
